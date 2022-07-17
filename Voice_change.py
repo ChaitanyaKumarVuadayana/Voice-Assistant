685739bbccd8815dfcb2b211f3453b023648728b
Chaitanya Kumar Vudayana <96481803+ChaitanyaKumarVuadayana@users.noreply.github.com>
2022-07-17 21:06:25 +0530

    How to change voice
    Added comment to define voice change between Men and Women

--- Voice_change.py
+++ Voice_change.py
@@ -4,6 +4,7 @@ import pyttsx3 as p3
 listener = sr.Recognizer()
 engine = p3.init()
 voices = engine.getProperty("voices")
+# to change voice we have to put 0(for Men vocals) and 1(for Women vocals) at the place of voices[x]
 engine.setProperty("voice", voices[1].id)
 engine.say("This is jarvis, personal assigtent of Iron man")
 engine.runAndWait()
